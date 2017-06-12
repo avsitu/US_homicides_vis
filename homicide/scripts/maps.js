6eec764cac81fb4c92283bd3410425dd5f7e0b0e
tsmorisawa <tsmorisawa@ucdavis.edu>
2017-06-12 11:32:48 -0700

    Initial combination of visualizations
    Parallel coordinates is not in yet

---
+++ homicide/scripts/maps.js
@@ -0,0 +1,302 @@
+var width = 960;
+var height = 500;
+
+var selectedStates = new Set()
+
+function stateDeselect(update) {
+    if(selectedStates.size > 0){
+        d3.selectAll(".selected").attr("class", "state bordered");
+        selectedStates.clear();
+        
+        stateFilter = [];
+        if(update){
+            updateChords();
+        }
+    }
+}
+
+function stateSelect(e, d) {
+    // highlight it 
+    d3.select(e.target).attr("class", "state bordered selected");
+    
+    if (selectedStates.has(d.properties.name)){
+        selectedStates.delete(d.properties.name)
+        
+        // unhighlight it
+        d3.select(e.target).attr("class", "state bordered");
+    }else{
+        selectedStates.add(d.properties.name)
+        //svg.select("g#"+d.properties.id)
+        //    .attr("fill", "#FFFFFF")
+    }
+    
+    //console.log(selectedStates)
+    stateFilter = [];
+    selectedStates.forEach(v => stateFilter.push(v))
+    updateChords()
+}
+
+
+// D3 Projection
+var projection = d3.geoAlbersUsa()
+				   .translate([width/2, height/2])    // translate to center of screen
+				   .scale([1000]);          // scale things down so see entire US
+        
+// Define path generator
+var path = d3.geoPath()               // path generator that will convert GeoJSON to SVG paths
+		  	 .projection(projection);  // tell path generator to use albersUsa projection
+
+var lowColor = '#ffe5e5'
+var highColor = '#7f0000'
+		
+//Create SVG element and append map to the SVG
+var svg = d3.select(".map")
+			.append("svg")
+			.attr("width", width)
+			.attr("height", height);
+        
+// Select tooltip from DOM
+var tooltip = d3.select("#tooltip");
+
+// Load in my states data!
+d3.csv("data/homicide_states.csv", function(data) {
+	var dataArray = [];
+	for (var d = 0; d < data.length; d++) {
+		dataArray.push(parseFloat(data[d].incidents))
+	}
+	var minVal = d3.min(dataArray)
+	var maxVal = d3.max(dataArray)
+	//console.log(minVal)
+	//console.log(maxVal)
+	var ramp = d3.scalePow().exponent(0.15).domain([minVal,maxVal]).range([lowColor,highColor])
+
+// Load GeoJSON data and merge with states data
+d3.json("data/us-states.json", function(json) {
+// Loop through each state data value in the .csv file
+for (var i = 0; i < data.length; i++) {
+
+	// Grab State Name
+	var dataState = data[i].state;
+
+	// Grab data value 
+	var dataValue = data[i].incidents;
+
+	// Find the corresponding state inside the GeoJSON
+	for (var j = 0; j < json.features.length; j++)  {
+		var jsonState = json.features[j].properties.name;
+		if (dataState == jsonState) {
+
+		// Copy the data value into the JSON
+		json.features[j].properties.incidents = dataValue; 
+
+		// Stop looking through the JSON
+		break;
+		}
+	}
+}
+	//console.log("BEFORE LEGEND")
+		// add a legend
+	var w = 140, h = 300;
+
+	var key = d3.select(".map")
+		.append("svg")
+		.attr("width", w)
+		.attr("height", h)
+		.attr("class", "legend");
+		
+	var legend = key.append("defs")
+			.append("svg:linearGradient")
+			.attr("id", "gradient")
+			.attr("x1", "100%")
+			.attr("y1", "0%")
+			.attr("x2", "100%")
+			.attr("y2", "100%")
+			.attr("spreadMethod", "pad");
+
+		legend.append("stop")
+			.attr("offset", "0%")
+			.attr("stop-color", highColor)
+			.attr("stop-opacity", 1);
+			
+		legend.append("stop")
+			.attr("offset", "100%")
+			.attr("stop-color", lowColor)
+			.attr("stop-opacity", 1);	
+
+		key.append("rect")
+		.attr("width", w - 100)
+		.attr("height", h)
+		.style("fill", "url(#gradient)")
+		.attr("transform", "translate(0,10)");
+
+		var y = d3.scalePow().exponent(.15)
+			.range([h, 0])
+			.domain([minVal, maxVal]);
+
+		var yAxis = d3.axisRight(y)
+
+		key.append("g")
+			.attr("class", "y axis")
+			.attr("transform", "translate(41,10)")
+			.call(yAxis)
+ // Bind the data to the SVG and create one path per GeoJSON feature
+//console.log("BEFORE CLICK CODE")
+svg.selectAll("path")
+	.data(json.features)
+	.enter()
+	.append("path")
+	.attr("d", path)
+	.style("stroke", "#fff")
+	.style("stroke-width", "1.5")
+	.style("fill", function(d) {return ramp(d.properties.incidents) })
+
+	.on('click', (d) => {
+	      if(!d3.event.ctrlKey && !d3.event.shiftKey){
+	      		stateDeselect(false); //deselect previous
+	      }
+	      stateSelect(d3.event, d);
+	      d3.event.stopPropagation();
+    })
+	.on("mouseover", function(d) {
+    	tooltip.transition()        
+      	   .duration(200)      
+           .style("opacity", .9)   
+        tooltip.html(d.properties.name + "<br/>"  + d.properties.incidents + " incidents")
+           .style("left", (d3.event.pageX - 50) + "px")     
+           .style("top", (d3.event.pageY - 55) + "px");    
+	})
+    .on('mousemove', () => {
+        tooltip.style('left', (d3.event.pageX - 50)+'px')	
+          .style('top', (d3.event.pageY - 55)+'px');
+    })
+
+    // fade out tooltip on mouse out               
+    .on("mouseout", function(d) {       
+        tooltip.transition()        
+           .duration(500)      
+           .style("opacity", 0);   
+    });
+})
+});
+//console.log("AFTER CLICK CODE")
+	/*
+	.on("click", function(d){
+
+		d3.csv("data/" + d.properties.name + ".csv", function(da) {
+		d3.selectAll(".legend").remove();
+		d3.selectAll(".arc").remove();
+		var width = 600,
+	    height = 600,
+	    radius = Math.min(width, height) / 2;
+	    var legendRectSize = 18;
+		var legendSpacing = 2;
+
+	    var color = d3.scale.ordinal()
+  		.range([ "#3182bd",
+				"#6baed6",
+				"#9ecae1",
+				"#c6dbef",
+				"#e6550d",
+				"#fd8d3c",
+				"#fdae6b",
+				"#fdd0a2",
+				"#31a354",
+				"#74c476",
+				"#a1d99b",
+				"#c7e9c0",
+				"#756bb1",
+				"#9e9ac8",
+				"#bcbddc",
+				"#dadaeb",
+				"#636363",
+				"#969696",
+				"#bdbdbd",
+				"#d9d9d9",
+				"#637939",
+				"#8ca252",
+				"#b5cf6b",
+				"#cedb9c",
+				"#8c6d31",
+				"#e7ba52",
+				"#e7cb94",
+				"#843c39",
+				"#ad494a",
+				"#d6616b",
+				"#e7969c",
+				"#7b4173",
+				"#a55194",
+				"#ce6dbd",
+				"#de9ed6"]);
+
+  		var svg = d3.select('#donutchart')
+		  .select('svg')
+		  .attr('width', width)
+		  .attr('height', height)
+		  .append('g')
+		  .attr('transform', 'translate(' + (width / 2) +  ',' + (height / 2) + ')');
+
+		var arc = d3.svg.arc()
+  		.innerRadius(radius-70)
+  		.outerRadius(radius-10);
+
+  		var pie = d3.layout.pie()
+		  .value(function(d) { return d.duration; })
+		  .sort(function(a, b) {
+		return a.shape.localeCompare(b.shape);
+		});
+		 
+		var g = svg.selectAll(".arc")
+		    .data(pie(da))
+		    .enter().append("g")
+		    .attr("class", "arc");
+
+		g.append("path")
+		    .attr("d", arc)
+		    .style("fill", function (d) {
+		    return color(d.data.shape);
+		    });
+
+		var legend = svg.selectAll('.legend')
+		  .data(color.domain())
+		  .enter()
+		  .append('g')
+		  .attr('class', 'legend')
+		  .attr('transform', function(d, i) {
+		    var height = legendRectSize + legendSpacing;
+		    var offset =  height * color.domain().length / 2;
+		    var horz = -2 * legendRectSize;
+		    var vert = i * height - offset;
+		    return 'translate(' + horz + ',' + vert + ')';
+		  });
+		legend.append('rect')
+		  .attr('width', legendRectSize)
+		  .attr('height', legendRectSize)
+		  .style('fill', color)
+		  .style('stroke', color);
+
+		legend.append('text')
+		  .attr('x', legendRectSize + legendSpacing)
+		  .attr('y', legendRectSize - legendSpacing)
+		  .text(function(d) { return d; });
+      })
+	})
+	.on("mouseover", function(d) {
+    	div.transition()        
+      	   .duration(200)      
+           .style("opacity", .9);      
+           div.html(d.properties.name + "<br/>"  + d.properties.incidents)
+           .style("left", (d3.event.pageX) + "px")     
+           .style("top", (d3.event.pageY - 28) + "px");    
+	})   
+
+    // fade out tooltip on mouse out               
+    .on("mouseout", function(d) {       
+        div.transition()        
+           .duration(500)      
+           .style("opacity", 0);   
+    });
+	;
+	});
+	console.log("after CLICKed")
+*/
+
---
+++ homicide/scripts/pc.js
@@ -0,0 +1,210 @@
+var pcm = [100, 40, 50, 300],
+  pcw = 1700 - pcm[1] - pcm[3],
+  pch = 800 - pcm[0] - pcm[2];
+var pcline = d3.line();
+var pcdragging = {};
+var pcx2 = d3.scaleBand().range([0, pcw]);
+var pcy2 = {};
+
+var pcsvg2 = d3.select('.pc')
+  .attr("width", pcw + pcm[1] + pcm[3])
+  .attr("height", pch + pcm[0] + pcm[2]).append("g")
+  .attr("transform", "translate(" + 150 + "," + 50 + ")");
+
+var pcbackground = pcsvg2.append('g').attr('class', 'background')
+  .attr('width', pcw)
+  .attr('height', pch);
+var pcforeground = pcsvg2.append('g').attr('class', 'foreground')
+  .attr('width', pcw)
+  .attr('height', pch);;
+
+var pclegend2 = d3.select('.pc').append('g').attr('class', 'legend2').attr('transform', 'translate(1050,-430)'); 
+var pccolors2 = ["#7fcdbb","#41b6c4","#1d91c0","#225ea8","#253494","#081d58"];
+var pcfirst = true;
+var dimensions;
+
+function pc(data) {
+	//console.log('pc')
+	// d3.csv(fn, myfilter, function(data) { 
+	// 	var axis = d3.axisLeft();
+	// br_min = d3.min(data, function(p) { return p['bedrooms']; });
+	// 	br_max = d3.max(data, function(p) { return p['bedrooms']; });
+
+	// Extract the list of dimensions and create a scale for each.
+    dimensions = d3.keys(data[0]).filter(function(d) {
+		if(d == 'month' || d == 'year' || d == 'state') return false;
+		else if(d == 'v_age' || d == 'p_age') {
+		  	pcy2[d] = d3.scaleLinear().domain([d3.min(data, function(p) { return p[d]; }),d3.max(data, function(p) { return p[d]; })])
+		  	.range([pch, 0]); 
+					
+			return true;
+		}
+		else {
+		  	pcy2[d] = d3.scalePoint().domain(data.map(function(p) {
+	      		return p[d];
+	    	})).range([pch, 0]); 			
+		  	return true;
+		} 	
+    })
+	pcx2.domain(dimensions);
+	// price_color = d3.scaleQuantile()
+	// .domain([d3.min(data, function(d) { return d.price; }),d3.max(data, function(d) { return d.price; })])
+	// .range(colors2);
+
+  	// background = background.selectAll("path")
+   //    .data(data)
+   //  .enter().append("path")
+   //    .attr("d", path).attr('stroke','#ddd').attr('fill','none');	
+
+	var update = pcforeground.selectAll("path")
+	  .data(data);	
+
+	  update.exit().transition().duration(1000)
+      .style("opacity", 0).remove();
+	  
+	  update.enter().append("path").merge(update).attr("class", "enter").attr('fill','none').attr('stroke-width',1).attr('stroke','steelblue')
+		.attr("d", path).style('opacity',0)
+		.transition().duration(1000)
+      	.style("opacity", 1);
+
+	  // var g = svg2.selectAll(".dimension")
+	  //     .data(dimensions)
+	  //   .enter().append("g")
+	  //     .attr("class", "dimension")
+	  //     .attr("transform", function(d) { return "translate(" + x2(d) + ")"; })
+	  //     .call(d3.drag()
+	  //       .origin(function(d) { return {x: x2(d)}; })
+	  //       .on("dragstart", function(d) {
+	  //         dragging[d] = x2(d);
+	  //         background.attr("visibility", "hidden");
+	  //       })
+	  //       .on("drag", function(d) {
+	  //         dragging[d] = Math.min(width, Math.max(0, d3.event.x));
+	  //         foreground.attr("d", path);
+	  //         dimensions.sort(function(a, b) { return position(a) - position(b); });
+	  //         x.domain(dimensions);
+	  //         g.attr("transform", function(d) { return "translate(" + position(d) + ")"; })
+	  //       })
+	  //       .on("dragend", function(d) {
+	  //         delete dragging[d];
+	  //         transition(d3.select(this)).attr("transform", "translate(" + x(d) + ")");
+	  //         transition(foreground).attr("d", path);
+	  //         background
+	  //             .attr("d", path)
+	  //           .transition()
+	  //             .delay(500)
+	  //             .duration(0)
+	  //             .attr("visibility", null);
+	  //       }));      	
+
+ 	var labels = ['Victim Sex', 'Perpetrator Sex', 'Victim Age', 'Perpetrator Age', 'Victim Race', 'Perpetrator Race', 'Relationship', 'Weapon'];
+ 	if(pcfirst) {
+ 		for(var d=0; d < dimensions.length; d++) {
+	 		pcsvg2.append('g').attr('class', "axis"+d)
+	 		.attr("transform", function() {
+    			return "translate(" + pcx2(dimensions[d]) + ")"; });
+	 		// if (labels[d] == 'Bedrooms') {
+	 			// console.log(br_min+' '+br_max);
+				d3.select('.axis'+d).call(d3.axisRight(pcy2[dimensions[d]]))
+				.append("svg:text")
+			  .attr("y", -9).attr('fill','black')
+		  	.text(labels[d]);
+	 		// }
+	 		// else { 			
+				// d3.select('.axis'+d).call(d3.axisRight(y2[dimensions[d]]))
+				// .append("svg:text")
+			 //  .attr("y", -9).attr('fill','black')
+		  // 	.text(labels[d]);
+	 		// }
+ 		}
+	 	pcfirst = false;
+ 	}
+ 	else {
+ 		for(var d=0; d < dimensions.length; d++) {
+ 			if(labels[d] == 'Bedrooms') {
+ 				// console.log(br_min+' '+br_max);
+ 				d3.select('.axis'+d).transition().duration(1000).call(d3.axisRight(pcy2[dimensions[d]]));
+ 			}
+			else
+				d3.select('.axis'+d).transition().duration(1000).call(d3.axisRight(pcy2[dimensions[d]]));
+ 		}
+ 	}
+
+ // 	update2 = legend2.selectAll("text")
+ //        .data([0].concat(price_color.quantiles()), function(d) { return d; });
+	// update2.exit().remove();
+ //  	update2.enter().append("text").merge(update2)
+ //      .attr("x", function(d, i) { return 70 * i + 5; })
+ //      .attr("y", h + 65)  
+ //      .text(function(d) { 
+ //      	val = Math.round(d).toString();
+ //      	if(val.length>=6) val = val.slice(0,3)+'k';
+ //      	else if(val.length==5) val = val.slice(0,2)+'k';
+ //      	return " >= $" + val; });
+ // 	update3 = legend2.selectAll("rect")
+ //        .data([0].concat(price_color.quantiles()), function(d) { return d; });        
+	// update3.exit().remove();
+	// update3.enter().append("rect").merge(update3)
+	//       .attr("x", function(d, i) { return 70 * i; })
+	//       .attr("y", h + 20)
+	//       .attr("width", 70)
+	//       .attr("height", 30)
+	//       .style("fill", function(d, i) { return colors2[i]; });   
+	// legend2.append('g').append('text').text('Sales Price:').attr('x',-20).attr('transform','translate(-60,510)');
+       
+};	
+
+function position(d) {
+  var v = pcdragging[d];
+  return v == null ? pcx2(d) : v;
+}
+
+function transition(g) {
+  return g.transition().duration(500);
+}
+
+// Returns the path for a given data point.
+function path(d) {
+  return pcline(dimensions.map(function(p) {
+    return [position(p), pcy2[p](d[p])];
+  }));
+}	
+
+/*function myfilter(d) {
+	return {
+		bedrooms: +d.BedroomAbvGr,
+		lot_area: +d.LotArea,
+		quality: +d.OverallQual,
+		built: +d.YearBuilt,
+		price: +d.SalePrice
+	};
+}*/
+
+function updatePC(selection) {	
+	d3.csv('data/pc_data_sample.csv',
+	function(d) {
+		return {
+			month: d['Month'],
+			year: +d['Year'],
+			state: d['State'],
+		  v_sex: d['Victim Sex'],
+		  p_sex: d['Perpetrator Sex'],
+		  v_age: +d['Victim Age'],
+		  p_age: +d['Perpetrator Age'],
+		  v_race: d['Victim Race'],
+		  p_race: d['Perpetrator Race'],
+		  rel: d['Relationship'],
+		  weap: d['Weapon']
+		};
+	},
+	function(error, mydata) {
+		if(error) throw error;
+		mydata = mydata.filter(function(d) {
+			if(d.state == selection[0]) return true;
+			else return false;
+		})
+		pc(mydata);
+	}
+	);
+}
+// updatePC(['Alaska']);
